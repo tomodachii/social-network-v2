ade82982f3b8ec7bd0799b5b3b94e1efdeaa752c
Hoang Do <tuanhoang2do2311@gmail.com>
2023-12-14 06:37:39 +0000

    file repo init

--- backend/apps/file-service/src/main.ts
+++ backend/apps/file-service/src/main.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import path from 'path'
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3006;
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: path.join(__dirname, '..', 'data') });
+  console.log(path.join(__dirname, '..', 'data'))
 });
 
 app.listen(port, host, () => {
