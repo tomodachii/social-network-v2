d9b561fcd715fe04f8b6f6728a40336f6a7f62c0
thangnv-0703 <sunshines0703@gmail.com>
2023-12-26 16:02:02 +0700

    add library data access of user service

---
+++ backend/libs/user/data-access-user/src/lib/prisma.user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { BioImageRecord, PrismaClient, UserRecord } from '@prisma/client/user';
+
+export * from '@prisma/client/user';
+export type UserPrersistent = UserRecord & {
+  avatar: BioImageRecord;
+  cover: BioImageRecord;
+} & { version?: number };
+
+@Injectable()
+export class PrismaUserService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaUserService.name);
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+      errorFormat: 'colorless',
+    });
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
