b9f103424f61a0a3a8ae4990b261a8522702082b
thangnv-0703 <sunshines0703@gmail.com>
2023-12-26 16:03:11 +0700

    remove database module

--- backend/apps/post-service/src/database/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaPostService } from './prisma.post.service';
-
-@Module({
-  providers: [PrismaPostService],
-  exports: [PrismaPostService],
-})
-export class DatabaseModule {}
