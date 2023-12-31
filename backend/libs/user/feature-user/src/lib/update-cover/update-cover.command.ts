20a0a8d23426c6c7ad1f05d2cc9be61a9b42cf12
thangnv-0703 <sunshines0703@gmail.com>
2023-12-31 10:53:03 +0000

    update use case update cover image in user service

--- backend/libs/user/feature-user/src/lib/update-cover/update-cover.command.ts
+++ backend/libs/user/feature-user/src/lib/update-cover/update-cover.command.ts
@@ -2,14 +2,12 @@ import { Command, CommandProps } from '@lib/shared/ddd-v2';
 
 export class UpdateCoverCommand extends Command {
   readonly fileId: string;
-  readonly userId: string;
   readonly extension: string;
   readonly size: number;
 
   constructor(props: CommandProps<UpdateCoverCommand>) {
     super(props);
     this.fileId = props.fileId;
-    this.userId = props.userId;
     this.extension = props.extension;
     this.size = props.size;
   }
