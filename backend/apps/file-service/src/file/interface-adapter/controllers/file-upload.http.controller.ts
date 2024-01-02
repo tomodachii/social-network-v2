9232676a9521876bc3a5076d8ecdf5e28b1e0176
Hoang Do <tuanhoang2do2311@gmail.com>
2024-01-02 16:09:13 +0700

    shared file library (#13)
    * shared file domain entities
    
    * merge changes
    
    * file domain redesign
    
    * file service controller

--- backend/apps/file-service/src/file/interface-adapter/controllers/file-upload.http.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Router, Request, Response } from 'express';
-
-export const uploadRouter = Router();
-
-uploadRouter.post('/', (req: Request, res: Response) => {
-  res.send('File uploaded');
-});
--- backend/libs/shared/file/src/lib/domain/file.type.ts
+++ backend/libs/shared/file/src/lib/domain/file.type.ts
@@ -1,30 +1,24 @@
-export enum FILE_EXTENSION {
-  JPG = "jpg",
-  PNG = "png",
-  MP4 = "mp4",
-  TXT = "txt",
-  PDF = "pdf"
-}
-
 export enum FILE_TYPE {
   IMAGE = "images",
   VIDEO = "videos",
   DOCUMENT = "documents"
 }
 
+export enum IMAGE_FILE_EXTENSION {
+  JPG = "jpg",
+  PNG = "png",
+}
+
+export enum DOCUMENT_FILE_EXTENSION {
+  TXT = "txt",
+  PDF = "pdf"
+}
+
+export enum VIDEO_FILE_EXTENSION {
+  MP4 = "mp4",
+}
+
 export enum SERVICE_PREFIX {
   USER = "user",
   POST = "post",
 }
-
-export type ServicePrefix = string
-
-export type ImageExtension = FILE_EXTENSION.JPG | FILE_EXTENSION.PNG;
-export type VideoExtension = FILE_EXTENSION.MP4;
-export type DocumentExtension = FILE_EXTENSION.TXT | FILE_EXTENSION.PDF;
-
-export type FilePath =
-  | { type: FILE_TYPE.IMAGE, servicePrefix: ServicePrefix, ext: ImageExtension, name: String }
-  | { type: FILE_TYPE.VIDEO, servicePrefix: ServicePrefix, ext: VideoExtension, name: String }
-  | { type: FILE_TYPE.DOCUMENT, servicePrefix: ServicePrefix, ext: DocumentExtension, name: String };
-
