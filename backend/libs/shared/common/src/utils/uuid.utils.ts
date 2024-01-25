17bd92e9b97124da1e43676787b56616a8586312
thangnv-0703 <sunshines0703@gmail.com>
2024-01-25 13:18:50 +0000

    Add  utility functions

---
+++ backend/libs/shared/common/src/utils/uuid.utils.ts
@@ -0,0 +1,14 @@
+import { v4 as uuid, validate } from 'uuid';
+
+/**
+ * Generate uuid v4.
+ */
+export const generateUUID = (): string => {
+  return uuid();
+};
+
+/**
+ * Check if input is uuid v4
+ * @param input
+ */
+export const isUUID = (input: unknown): boolean => validate(input as string);
